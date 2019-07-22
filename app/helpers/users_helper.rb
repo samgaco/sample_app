b0ceb4deea9b7753617d74032fb8f2c5b8e426ac
Samuel <samuelgarciacompanys@gmail.com>
2019-07-22 12:36:17 +0200

    Implement advanced login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,8 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  # test "authenticated? should return false for a user with nil digest" do
+  #   assert_not @user.authenticated?('')
+  # end
 end
